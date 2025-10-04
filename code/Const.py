a812663b6e56562fb4663614687f4b780758f876
Mateus Chagas <=>
2025-10-04 13:28:57 -0300

    Creating Player Shot Creating Enemy Shot

--- code/Const.py
+++ code/Const.py
@@ -9,19 +9,6 @@ COLOR_YELLOW = (255, 255, 0)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
-ENTITY_SPEED = {
-    'Level1Bg0': 0,
-    'Level1Bg1': 1,
-    'Level1Bg2': 2,
-    'Level1Bg3': 3,
-    'Level1Bg4': 4,
-    'Level1Bg5': 5,
-    'Level1Bg6': 6,
-    'Player1': 3,
-    'Player2': 3,
-    'Enemy1': 2,
-    'Enemy2': 1,
-}
 
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
@@ -44,6 +31,31 @@ ENTITY_HEALTH = {
     'Enemy1Shot': 1,
     'Enemy2': 60,
     'Enemy2Shot': 1,
+}
+
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Player1': 3,
+    'Player1Shot': 4,
+    'Player2': 3,
+    'Player2Shot': 5,
+    'Enemy1': 2,
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
+}
+
+ENTITY_SHOT_DELAY = {
+    'Player1': 15,
+    'Player2': 10,
+    'Enemy1': 100,
+    'Enemy2': 200,
 }
 
 # G
