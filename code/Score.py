c450660bd811ebe160f4ce1433149193f108fdc8
Mateus Chagas <=>
2025-10-07 12:45:43 -0300

    Start implementation of DBProxy and score save in DB

--- code/Score.py
+++ code/Score.py
@@ -3,6 +3,7 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from code.Const import WIN_WIDTH, C_WHITE, GAME_TITLE, C_YELLOW, SCORE_POS, MENU_OPTION
+from code.DBProxy import DBProxy
 
 
 class Score:
@@ -12,14 +13,33 @@ class Score:
         self.surf = pygame.image.load('./asset/ScoreBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def save(self, game_mode: str, player_scor: list[int]):
+    def save(self, game_mode: str, player_score: list[int]):
         pygame.mixer_music.load('./asset/Score.mp3')
         pygame.mixer_music.play(-1)  # parametro -1 faz a musica tocar infinitamente
-        self.window.blit(source=self.surf, dest=self.rect)  # primeiro desenha o background depois desenha o texto
+        db_proxy = DBProxy('DBScore')
         while True:
+            self.window.blit(source=self.surf, dest=self.rect)  # primeiro desenha o background depois desenha o texto
             self.score_text(48, 'YOU WIN!!!', C_YELLOW, SCORE_POS['Title'])
             if game_mode == MENU_OPTION[0]:
-                text = 'Player 1 enter your name (4 characters):'
+                score = player_score[0]
+                text = 'Enter Player 1 name (4 characters):'
+            if game_mode == MENU_OPTION[1]:
+                score = (player_score[0]+player_score[1]) / 2
+                text = 'Enter Team name (4 characters):'
+
+            if game_mode == MENU_OPTION[2]:
+                if player_score[0] > player_score[1]:
+                    score = player_score[0]
+                    text = 'Enter Player 1 name (4 characters):'
+                if player_score[1] > player_score[0]:
+                    score = player_score[1]
+                    text = 'Enter Player 2 name (4 characters):'
+                if player_score[1] == player_score[0]:
+                    score = player_score[0]
+                    text = 'Enter Player 1 and 2 name together (4 characters ):'
+
+
+
             pygame.display.flip()
             pass
 
